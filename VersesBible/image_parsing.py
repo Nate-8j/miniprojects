ecbc2bfdbf0ea8c9850908dafcfeb9b519aab135
Nate <iakov7548@gmail.com>
2025-09-04 22:25:43 +0300

    modified .gitignore

--- VersesBible/image_parsing.py
+++ VersesBible/image_parsing.py
@@ -1,17 +1,42 @@
-from VersesBible.config import KEY
+import time
 import requests
 
-query = "nature"
-count = 10
+from VersesBible.config import KEY
 
+url = f'https://api.unsplash.com/photos/random'
 
-url = f'https://api.unsplash.com/search/photos'
+headers = {
+    "Authorization": f"Client-ID {KEY}",
+}
 params = {
-    'query': query,
-    'per_page': count,
-    'client_id': KEY,
+    'query': "nature",
+    'count': 30
 }
 
-response = requests.get(url, params=params)
-photos = response.json()
-print(photos)
+usr_ids = set()
+
+total = 404
+cnt = 257
+for i in range(cnt, total+1):
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        data = response.json()
+        for j, photo in enumerate(data):
+            photo_id = photo['id']
+            if photo_id in usr_ids:
+                continue
+            else:
+                usr_ids.add(photo_id)
+            photo_url = photo['urls']['full'] + "&w=1080&h=1080&fit=crop"
+            response = requests.get(photo_url, stream=True)
+            with open(rf"C:\Users\Renat\Pictures\VersesBible\image{cnt}.jpg", "wb") as f:
+                for chunk in response.iter_content(1024):
+                    f.write(chunk)
+
+            print(f"{cnt}/{total} Скачано: image_{cnt}.jpg")
+            cnt += 1
+            time.sleep(3)
+
+    except Exception as e:
+        print("Ошибка: ", e)
+        time.sleep(5)
